3c50a0ad61163c656e2d216a66513c8649e60b3c
user <user@DESKTOP-P8C9DP4.home>
2024-02-16 10:04:55 +0200

    sdgac

---
+++ Object Repository/Page_Google/div_Accept tot.rs
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Accept tot</name>
+   <tag></tag>
+   <elementGuidId>94b74478-3b3e-4141-931f-de38aed27034</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#L2AGLb > div.QS5gu.sy4vM</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='L2AGLb']/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>9f7ad228-25ab-4d71-8c92-0fb43e267663</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>QS5gu sy4vM</value>
+      <webElementGuid>542ca767-364b-4f90-9d04-d83b46999700</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>none</value>
+      <webElementGuid>6f2170dd-7351-47e4-8cd8-279f52d9b051</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Acceptă tot</value>
+      <webElementGuid>42bcf221-953b-4262-9ba9-bf8527501aa3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;L2AGLb&quot;)/div[@class=&quot;QS5gu sy4vM&quot;]</value>
+      <webElementGuid>88e1a4ad-e4fd-42cd-9a60-c746811b2662</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//button[@id='L2AGLb']/div</value>
+      <webElementGuid>513aa85a-f67c-4539-a551-26f9d9aa4bcb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Respinge tot'])[1]/following::div[1]</value>
+      <webElementGuid>2623229a-5642-483b-bcfa-416b2ea44c68</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='g.co/privacytools/ro'])[1]/following::div[4]</value>
+      <webElementGuid>5dda926c-2266-4319-87a6-08c1f6235065</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Mai multe opțiuni'])[1]/preceding::div[1]</value>
+      <webElementGuid>c1b7cb63-2cea-42ea-9937-86c539117c1d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Confidențialitate'])[2]/preceding::div[2]</value>
+      <webElementGuid>99c1b26e-6d56-453b-a2c4-bda360bf14fc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Acceptă tot']/parent::*</value>
+      <webElementGuid>52973d06-54fe-4f75-a5b2-445c83331fc7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button[2]/div</value>
+      <webElementGuid>4d0717d5-48e0-4a4c-b6e1-e9dcb4f1113c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Acceptă tot' or . = 'Acceptă tot')]</value>
+      <webElementGuid>67d4c996-3d0f-4d91-a893-ab83888e33dd</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
